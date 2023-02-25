7cba28da7347f891021725adbf57d0c7208e6c7b
Mariano Manzo <marianomanzo@MacBook-Pro-de-Mariano.local>
2023-02-25 20:38:20 -0300

    fase 1.5

--- funciones.js
+++ funciones.js
@@ -1,11 +1,40 @@
 
+  function openWhatsappChat() {
+    window.open("https://wa.me/1234567890", "_blank");
+  }
 
-/*document.getElementById('boton').onclick = function () {
+var pepe = ''
 
-    console.log("capturamos el elemento clic");
-    document.getElementById("demo").innerHTML ="estamos probando un evento";
-}
-*/
+  var booleano = false; // variable booleana inicial con valor "true"
+
+  function cambiarValor() {
+    if (booleano) {
+      booleano = false; // si la variable es "true", cambia a "false"
+     pepe = "" ;
+    } else {
+      booleano = true; // si la variable es "false", cambia a "true"
+     pepe = 'https://randomuser.me/api/' ;
+    }
+  
+
+
+fetch(pepe)
+.then(response => response.json())
+.then(data => {
+    const user = data.results[0];
+    document.getElementById('user-avatar').src = user.picture.large;
+    document.getElementById('user-name').textContent = `${user.name.first} ${user.name.last}`;
+    document.getElementById('user-email').innerHTML = `<i class="fas fa-envelope"></i> ${user.email}`;
+    document.getElementById('user-phone').innerHTML = `<i class="fas fa-phone"></i> ${user.phone}`;
+    document.getElementById('user-location').innerHTML = `<i class="fas fa-map-marker-alt"></i> ${user.location.city}, ${user.location.country}`;
+
+    const experienceList = document.getElementById('user-experience');
+    user.experience.forEach(exp => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${exp.position} at ${exp.company}`;
+        experienceList.appendChild(listItem)
+    }).reset()
+})}
 function enviarFormulario() {
     // Obtener los valores del formulario
     const nombre = document.getElementById('nombre').value;
@@ -32,10 +61,8 @@ function enviarFormulario() {
 
     return true;
   }
-
-  function openWhatsappChat() {
-    window.open("https://wa.me/1234567890", "_blank");
-  }
   
   // Agregamos un event listener al botón para que llame a la función openWhatsappChat() cuando se presione el botón.
   document.getElementById("whatsapp-button").addEventListener("click", openWhatsappChat);
+
+ 
